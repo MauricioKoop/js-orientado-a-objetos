817627d0d2ddbeb0ad326c8e20b662cbf1a328af
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2024-10-22 15:02:43 -0300

    cria uma classe para representar o nome, idade e a media de um determinado aluno.

---
+++ script3.js
@@ -0,0 +1,29 @@
+class aluno {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.notas = [];
+    }
+
+    adicionarNotas(nota) {
+        this.notas.push(nota);
+    }
+
+    calcularMedia() {
+        let somaNotas = this.notas.reduce((ant, atual) => { return ant + atual });
+        let mediaNotas = somaNotas / this.notas.length
+        return mediaNotas.toFixed(2);
+    }
+
+    escrever() {
+        console.log(`Aluno: ${this.nome}, Idade: ${this.idade}, média escolar: ${this.calcularMedia()}`);
+    }
+}
+
+let alunoEduardo = new aluno("Anne", "23");
+alunoEduardo.adicionarNotas(10);
+alunoEduardo.adicionarNotas(9);
+alunoEduardo.adicionarNotas(9);
+
+alunoEduardo.calcularMedia();
+alunoEduardo.escrever();
